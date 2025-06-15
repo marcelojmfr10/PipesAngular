dbf7857001fc7f415af54c983d397bc1e3625ca7
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-06-15 17:34:40 -0600

    Pipes de texto, número y fecha, así como el cambio de idiomas

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  // changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent {
+
+  // recordar que siempre se puede desestructurar, por ejemplo title y path
+  routes = routes.map((route) => ({
+    title: route.title ?? '',
+    path: route.path ?? ''
+  }));
+
+
+
+
+
+ }
--- src/app/pages/numbers-page/numbers-page.component.ts
+++ src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './numbers-page.component.html',
   // changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class NumbersPageComponent { }
+export default class NumbersPageComponent {
+
+  totalSells = signal(24561262.5658);
+  percent = signal(0.4598);
+
+}
