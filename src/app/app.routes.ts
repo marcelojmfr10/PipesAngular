3f574fd8c4ebc76cf3fad77c73bf8b68bfb2ca65
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-06-15 16:32:33 -0600

    Estructura de directorios, rutas y daisyUI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'basic',
+    title: 'Pipes Básicos',
+    loadComponent: () => import('./pages/basic-page/basic-page.component'),
+  },
+  {
+    path: 'numbers',
+    title: 'Numbers Pipe',
+    loadComponent: () => import('./pages/numbers-page/numbers-page.component'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes no tan comúnes',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page.component'),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes Personalizados',
+    loadComponent: () => import('./pages/custom-page/custom-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic',
+  }
+
+];
