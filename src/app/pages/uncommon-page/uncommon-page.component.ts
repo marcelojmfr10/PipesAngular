1bf21b43750d9290098cef078ce5de7003f20c2c
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-06-16 19:19:38 -0600

    Finalización pipes

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,85 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CardComponent } from "../../components/card/card.component";
+import { AsyncPipe, I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { interval, map, tap } from 'rxjs';
+
+const client1 = {
+  name: 'marcelo',
+  gender: 'male',
+  age: 28,
+  address: 'guatemala'
+}
+
+const client2 = {
+  name: 'julia',
+  gender: 'female',
+  age: 30,
+  address: 'guatemala'
+}
+
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe, JsonPipe,
+    UpperCasePipe, KeyValuePipe, TitleCasePipe, AsyncPipe],
   templateUrl: './uncommon-page.component.html',
   // changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+  //i18nSelect
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  }
+
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
+
+  //i18nPlural
+  clientsMap = signal({
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    other: 'tenemos # clientes esperando'
+  });
+
+  clients = signal([
+    'maria', 'pedro', 'jose', 'natalia', 'jorge', 'orlando', 'hugo', 'julia', 'ana'
+  ]);
+
+  deleteClient() {
+    this.clients.update(prev => prev.slice(1));
+  }
+
+  //KeyValue
+  profile = {
+    name: 'Marcelo',
+    age: 28,
+    address: 'Guatemala'
+  };
+
+  //Async
+  promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('tenemos data en la promesa');
+      // reject('tenemos error en la data');
+      console.log('promesa finalizada');
+    }, 3500);
+  });
+
+  myObservableTimer = interval(2000).pipe(
+    map((value) => value + 1),
+    tap((value) => console.log('tap', value))
+  );
+
+
+}
